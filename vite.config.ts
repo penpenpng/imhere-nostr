bf2f726ba6ab917bd4eefab30c11570debea7209
penpenpng <poman1638m@gmail.com>
2023-10-17 20:50:26 +0900

    deps: install svelte-leaflet

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import postcss from "rollup-plugin-postcss";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte({
+      emitCss: true,
+    }),
+    postcss({
+      extract: true,
+    }),
+  ],
+});
