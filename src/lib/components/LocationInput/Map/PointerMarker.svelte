3f6cc60a60cb9c942b0b4ef24bbc8798c499c997
penpenpng <poman1638m@gmail.com>
2023-10-21 11:19:23 +0900

    fix: handle error on reversing

--- src/lib/components/LocationInput/Map/PointerMarker.svelte
+++ src/lib/components/LocationInput/Map/PointerMarker.svelte
@@ -7,16 +7,27 @@
   export let mapPoint: MapPoint;
 
   const dispatch = createEventDispatcher<{ input: { mapPoint: MapPoint } }>();
+
+  function displayLatLng(latLng: [number, number]) {
+    const [lat, lng] = latLng.map((v) => Math.round(v * 10 ** 6) / 10 ** 6);
+    return `(${lat}, ${lng})`;
+  }
 </script>
 
 <Marker latLng={mapPoint.latLng}>
   {#await mapPoint.place}
     Loading...
   {:then place}
-    <b>{place.name ?? "No Data"}</b>
+    {#if place.name}
+      <b>{place.name}</b>
+    {:else}
+      <b class="has-text-grey-light">{displayLatLng(place.latLng)}</b>
+    {/if}
+    <br />
     {place.display_name ?? ""}
-    <button class="button" on:click={() => dispatch("input", { mapPoint })}
-      >Post</button
+    <button
+      class="button is-info is-small is-fullwidth"
+      on:click={() => dispatch("input", { mapPoint })}>Post</button
     >
   {:catch err}
     <span style="color:red;">{err}</span>
--- src/lib/nominatim.ts
+++ src/lib/nominatim.ts
@@ -15,21 +15,32 @@ export async function search(q: string): Promise<Place[]> {
 
 /** https://nominatim.org/release-docs/latest/api/Reverse/ */
 export async function reverse([lat, lon]: [number, number]): Promise<Place> {
-  const x = await get<RawPlace>("https://nominatim.openstreetmap.org/reverse", {
-    lat: `${lat}`,
-    lon: `${lon}`,
-    format: "jsonv2",
-  });
+  const x = await get<RawPlace | { error: string }>(
+    "https://nominatim.openstreetmap.org/reverse",
+    {
+      lat: `${lat}`,
+      lon: `${lon}`,
+      format: "jsonv2",
+    }
+  );
 
-  return toPlace(x);
+  if ("error" in x) {
+    return {
+      place_id: `${lat},${lon}`,
+      latLng: [lat, lon],
+      geohash: Geohash.encode(lat, lon),
+    };
+  } else {
+    return toPlace(x);
+  }
 }
 
 export interface Place {
   place_id: string;
   latLng: [number, number];
   geohash: string;
-  name: string;
-  display_name: string;
+  name?: string;
+  display_name?: string;
 }
 
 interface RawPlace {
