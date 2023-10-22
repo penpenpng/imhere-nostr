6be7fa787e0bce38550ef04d2d37c534328b3789
penpenpng <poman1638m@gmail.com>
2023-10-23 00:45:42 +0900

    feat: add footer

--- src/App.svelte
+++ src/App.svelte
@@ -13,11 +13,30 @@
   }
 </script>
 
-{#if showModal}
-  <PostNoteModal {place} on:close={() => void (showModal = false)} />
-{/if}
 <LocationInput
   on:input={(ev) => {
     openPostNoteModal(ev.detail.mapPoint);
   }}
 />
+
+<footer class="mx-4 mt-6 has-text-centered is-size-7 has-text-grey">
+  Author: <a
+    href="https://njump.me/npub133vj8ycevdle0cq8mtgddq0xtn34kxkwxvak983dx0u5vhqnycyqj6tcza"
+    target="_blank"
+  >
+    penpenpng
+  </a>, Source:
+  <a href="https://github.com/penpenpng/imhere-nostr" target="_blank">
+    GitHub
+  </a>, License:
+  <a
+    href="https://github.com/penpenpng/imhere-nostr/blob/main/LICENSE"
+    target="_blank"
+  >
+    MIT
+  </a>
+</footer>
+
+{#if showModal}
+  <PostNoteModal {place} on:close={() => void (showModal = false)} />
+{/if}
